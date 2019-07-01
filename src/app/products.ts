db225cc8f83eb6a47a43d1eddc5526e0611eaf98
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-01 13:25:40 +0530

    changes done

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,21 @@
 export const products = [
     {
-      name: 'Phone XL',
+      name: '1. PHONE - XL',
       price: 799,
       description: 'A large phone with one of the best screens'
     },
+
+
     {
-      name: 'Phone Mini',
+      name: '2. PHONE - MINI',
       price: 699,
       description: 'A great phone with one of the best cameras'
     },
+
+
     {
-      name: 'Phone Standard',
+      name: '3. PHONE STANDARD',
       price: 299,
-      description: ''
+      description: 'A great phone with great durability'
     }
   ];
